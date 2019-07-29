7b5e252108c2881c7205ce5b95f888d34016c51c
Khanh Luu <luukhanh96@gmail.com>
2019-07-30 01:47:21 +0200

    add user profile in navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducer';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  authSubscription: Subscription;
+  isLoading = false;
+  isLoggedin = false;
 
-  constructor() { }
+  constructor(private store: Store<fromApp.AppState>) {}
 
   ngOnInit() {
+    this.authSubscription = this.store.select('auth').subscribe(authState => {
+      this.isLoading = authState.isLoading;
+      this.isLoggedin = authState.user !== null;
+    });
   }
-
 }
